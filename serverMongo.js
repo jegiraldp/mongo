9b59255276b4407e625c743917b8147c872f5b81
jegiraldp <jegiraldp@gmail.com>
2020-07-03 19:50:33 -0500

    03jul2020_V2

--- serverMongo.js
+++ serverMongo.js
@@ -14,6 +14,7 @@ require("./config/passport.js");
 //use
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(express.static('public'));
 
 app.use(session({
   secret:'jorgegiraldo',
